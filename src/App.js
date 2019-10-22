5fca6f7c3a6e79f6a8f54dd80e7e8929d3992ee0
JonoAugustine <jaugust8@jhu.edu>
2019-10-22 18:47:26 -0400

    removed css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Content from './components/Content'
 import Project from './components/Project/Project'
-
-import './assets/style/root.css'
 
 class App extends React.Component {
 
